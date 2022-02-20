611cf4a5835fa86f43b113959be7710426bc3540
zfletcher-dot <77692515+zfletcher-dot@users.noreply.github.com>
2022-02-19 20:23:08 -0800

    Ringer Setting

--- app/src/main/java/com/example/intuith_java/MainActivity.java
+++ app/src/main/java/com/example/intuith_java/MainActivity.java
@@ -18,10 +18,23 @@ import com.example.intuith_java.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    Button mode,ring,vibrate,silent;
+    private AudioManager myAudioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +54,58 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        setContentView(R.layout.activity_main);
+
+        vibrate=(Button)findViewById(R.id.button3);
+        ring=(Button)findViewById(R.id.button2);
+
+        mode=(Button)findViewById(R.id.button);
+        silent=(Button)findViewById(R.id.button4);
+        myAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        vibrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                Toast.makeText(MainActivity.this,"Now in Ringing Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        silent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                Toast.makeText(MainActivity.this,"Now in silent Mode",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int mod=myAudioManager.getRingerMode();
+                if(mod==AudioManager.RINGER_MODE_VIBRATE){
+                    Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                            Toast.LENGTH_LONG).show();
+                } else if(mod==AudioManager.RINGER_MODE_NORMAL){
+                    Toast.makeText(MainActivity.this,"Now in Ringing Mode",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"Now in Vibrate Mode",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
@@ -73,4 +138,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
